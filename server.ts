13d01a006c68dd5013f19178de815efb99d473c4
Artem Belov <inctnce@icloud.com>
2020-10-29 17:35:33 +0300

    initial commit

---
+++ server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import path from "path";
+const app = express();
+const port = 3000;
+
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "client/build","index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
