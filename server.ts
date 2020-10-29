3149092492110857da3fada172bdb37745eeb1ef
Artem Belov <inctnce@icloud.com>
2020-10-29 18:28:22 +0300

    .

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get("/", (_req: any, res: any) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => {
